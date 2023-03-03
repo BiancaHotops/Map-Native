9e7634e49f68bb0daa69bcfbc671bf8051ba8a93
Bianca Hotops Alves <48944941831>
2023-03-03 11:30:04 -0300

    map 5 localidades

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -5,8 +5,15 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function MapScreen() {
-  const [latitude, setLatitude] = useState(-22.893210646979078)
-  const [ longitude, setLongitude] = useState(-47.050620170899485)
+
+  const list=[{nome: "Pão de Açúcar", lat:-22.89651152580656, long:-47.05166240434368 }, 
+                {nome: "Padaria Croissant D'or", lat:-22.896417969804343, long:-47.04965490430873},
+                {nome: "Shopping Iguatemi", lat:-22.8917626606111, long:-47.02762263024173},
+                {nome: "E.E. Carlos Gomes", lat:-22.896804076504957, long:-47.05712614981367},
+                {nome: "Outlet Cambuí", lat:-22.897634265788685, long:-47.04818903612985}]
+
+  // const [latitude, setLatitude] = useState(-22.893210646979078)
+  // const [ longitude, setLongitude] = useState(-47.050620170899485)
   return (
 <View style={styles.container}>
       <MapView style={styles.map} initialRegion={{
@@ -15,11 +22,20 @@ export default function MapScreen() {
       latitudeDelta: 0.003,
       longitudeDelta: 0.003,
     }}>
-      <Marker coordinate={{ latitude : latitude , longitude : longitude }}>
+
+{list.map((item)=> (
+    <Marker key={item.nome} coordinate={{latitude: item.lat, longitude: item.long}}>
+      <Callout>
+        <Text>{item.nome}</Text>
+      </Callout>
+      </Marker>
+  ))
+  }
+      {/* <Marker coordinate={{ latitude : latitude , longitude : longitude }}>
       <Callout>
         <Text>Confeitaria Romana</Text>
       </Callout>
-      </Marker>
+      </Marker> */}
     </MapView>
     </View>
  
